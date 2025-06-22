ae8b52c942107606bd3a3d0699fac721cb0cc0cd
othman-alojali <144936791+othman-alojali@users.noreply.github.com>
2025-06-22 20:56:11 +0200

    Refactor navigation and implement book management
    Updated navigation to focus on books, removing author management components. Introduced new book listing, adding, and editing pages. Enhanced database context to support book entities and updated related models. Adjusted database connection settings and migration files to reflect schema changes.

--- pro_se491/Data/Entities/Book.cs
+++ pro_se491/Data/Entities/Book.cs
@@ -3,10 +3,10 @@ namespace pro_se491.Data.Entities
     public class Book
     {
         public int Id { get; set; }
+        
         public string Title { get; set; }
-        public int AuthorId { get; set; }
-        public int BookId { get; set; }
-        public Author Author { get; set; }
-
+       
+        public string Author { get; set; }
+        public string Description { get; set; }
     }
 }
--- pro_se491/Data/Migrations/20250621204255_test.Designer.cs
+++ pro_se491/Data/Migrations/20250621204255_test.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pro_se491.Data;
 
@@ -11,9 +12,11 @@ using pro_se491.Data;
 namespace pro_se491.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250621204255_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -242,7 +245,7 @@ namespace pro_se491.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Book", (string)null);
+                    b.ToTable("Books", (string)null);
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
